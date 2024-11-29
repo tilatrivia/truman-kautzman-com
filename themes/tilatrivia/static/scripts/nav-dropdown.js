6fd877a424abdaca698c4bc78eafe5c19115c197
Truman Kautzman <tilatrivia@gmail.com>
2024-11-29 12:35:10 -0500

    Close menu on resize

--- themes/tilatrivia/static/scripts/nav-dropdown.js
+++ themes/tilatrivia/static/scripts/nav-dropdown.js
@@ -2,23 +2,10 @@
 document.addEventListener('keyup', () => console.log(document.activeElement))
 
 const nav = document.getElementById('tk-nav')
+const navContent = document.getElementById('tk-nav-content')
 const navOverlay = document.getElementById('tk-nav-overlay')
 
 let visible = false
-
-const setVisible = (newVisible) => {
-    visible = newVisible
-
-    // toggleClass(nav, 'h-min', 'h-0')
-    if (visible) {
-        nav.style.height = nav.scrollHeight + 'px'
-    } else {
-        nav.style.height = '0px'
-    }
-
-    toggleClass(navOverlay, 'bg-black/50', 'bg-black/0')
-    toggleClass(navOverlay, 'pointer-events-auto', 'pointer-events-none')
-}
 
 const toggleClass = (element, openClass, closedClass) => {
     if (visible) {
@@ -30,10 +17,26 @@ const toggleClass = (element, openClass, closedClass) => {
     }
 }
 
+const setVisible = (newVisible) => {
+    visible = newVisible
+
+    console.log(nav.scrollHeight)
+    
+    if (visible) {
+        nav.style.height = nav.scrollHeight + 'px'
+    } else {
+        nav.style.height = '0px'
+    }
+
+    toggleClass(navOverlay, 'bg-black/50', 'bg-black/0')
+    toggleClass(navOverlay, 'pointer-events-auto', 'pointer-events-none')
+}
+
 nav.addEventListener('focusin', () => setVisible(true))
 nav.addEventListener('focusout', () => setVisible(false))
 navOverlay.addEventListener('click', () => setVisible(!visible))
 document.getElementById('tk-nav-toggle').addEventListener('click', () => setVisible(!visible))
+window.addEventListener('resize', () => setVisible(false))
 
 
 setVisible(false)
